f35094cb24a10deb3182103cea38a53b1ee36205
CertifiedRac1st <penguin6705@gmail.com>
2025-06-07 17:42:01 +0630

    Add files via upload

---
+++ components/Spinner.tsx
@@ -0,0 +1,8 @@
+
+import React from 'react';
+
+const Spinner: React.FC = () => (
+  <div className="animate-spin rounded-full h-6 w-6 border-t-2 border-b-2 border-pink-500"></div>
+);
+
+export default Spinner;
